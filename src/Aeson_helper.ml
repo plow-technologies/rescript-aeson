34548cd228fdffdcb3f276b1b12b99674aa99867
James Haver <mchaver@gmail.com>
2017-11-30 17:05:49 +0800

    Add more TypeParameterRefs

--- src/Aeson_helper.ml
+++ src/Aeson_helper.ml
@@ -1,2 +1,20 @@
 type typeParameterRef0 =
   | TypeParameterRef0
+
+type typeParameterRef1 =
+  | TypeParameterRef1
+
+type typeParameterRef2 =
+  | TypeParameterRef2
+
+type typeParameterRef3 =
+  | TypeParameterRef3
+
+type typeParameterRef4 =
+  | TypeParameterRef4
+
+type typeParameterRef5 =
+  | TypeParameterRef5
+
+type typeParameterRef6 =
+  | TypeParameterRef6
