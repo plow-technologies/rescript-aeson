f0f7d3f468828d9062929d7364b74cd719d55d73
mchaver <mchaver@gmail.com>
2021-08-16 10:49:28 +0200

    Test encoding and decoding with custom map key

--- __tests__/Aeson_roundtrip_test.ml
+++ __tests__/Aeson_roundtrip_test.ml
@@ -11,10 +11,50 @@ let jsonRoundtripSpec decode encode json =
   let rDecoded = decode json in
   expect (resultMap encode rDecoded) |> toEqual (Belt.Result.Ok json)
 
+type pairKey =
+  | PairKey of (int * string)
+
+module PairKeyComparable =
+  Belt.Id.MakeComparable(
+    struct
+      type t = pairKey
+      let cmp a b =
+        (match (a, b) with
+         | (PairKey a, PairKey b) -> compare a b : int)
+    end
+  )
+
+type pairKeyMap =
+  { pairKeyMap : (pairKey, string, PairKeyComparable.identity) Belt.Map.t
+  }
+
+let encodePairKey x =
+  match x with
+  | PairKey y0 ->
+     Aeson.Encode.pair Aeson.Encode.int Aeson.Encode.string y0
+
+let decodePairKey json =
+  match Aeson.Decode.pair Aeson.Decode.int Aeson.Decode.string json with
+  | v -> Belt.Result.Ok (PairKey v)
+  | exception Aeson.Decode.DecodeError msg -> Belt.Result.Error ("decodePairKey: " ^ msg)
+
+let encodePairKeyMap (x: pairKeyMap) =
+ Aeson.Encode.object_
+   [ ( "pairKeyMap", Aeson.Encode.beltMap encodePairKey Aeson.Encode.string x.pairKeyMap )
+   ]
+
+let decodePairKeyMap json =
+  match Aeson.Decode.
+    { pairKeyMap = field "pairKeyMap" (beltMap (fun a -> unwrapResult (decodePairKey a)) Aeson.Decode.string ~id:(module PairKeyComparable)) json 
+    }
+  with
+  | v -> Belt.Result.Ok v
+  | exception Aeson.Decode.DecodeError msg -> Belt.Result.Error ("decodePairKey: " ^ msg)
+
 let () =
 
 describe "Belt.Map.String.t" (fun () ->
-  test "simple map" (fun () ->
+  test "string key map" (fun () ->
       jsonRoundtripSpec
         (Aeson.Decode.wrapResult (Aeson.Decode.beltMapString Aeson.Decode.string))
         (Aeson.Encode.beltMapString Aeson.Encode.string)
@@ -23,10 +63,19 @@ describe "Belt.Map.String.t" (fun () ->
 );
 
 describe "Belt.Map.Int.t" (fun () ->
-  test "simple map" (fun () ->
+  test "int key map" (fun () ->
       jsonRoundtripSpec
         (Aeson.Decode.wrapResult (Aeson.Decode.beltMapInt Aeson.Decode.string))
         (Aeson.Encode.beltMapInt Aeson.Encode.string)
         (Js.Json.parseExn "{\"1\":\"A\",\"2\":\"B\"}")
+    )
+);
+
+describe "(pairKey, string, PairKeyComparable.identity) Belt.Map.t" (fun () ->
+  test "custom key map" (fun () ->
+      jsonRoundtripSpec
+        decodePairKeyMap
+        encodePairKeyMap
+        (Js.Json.parseExn "{\"pairKeyMap\":[[[0,\"a\"],\"A\"],[[1,\"b\"],\"B\"]]}")
     )
 );
